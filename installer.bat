b1c3d73fd76f62e83ee33b4a6bc48bc0c3670227
Tvpattack <tvpattack@gmail.com>
2025-02-15 01:19:32 -0500

    push

---
+++ installer.bat
@@ -0,0 +1,14 @@
+title Installer Creator
+
+magicseteditor-a --create-installer ^
+    tvpsilverinstaller.mse-installer ^
+    magic.mse-game magic-m15.mse-style ^
+    magic-mana-large.mse-symbol-font ^
+    magic-mana-small.mse-symbol-font ^
+    magic-default-image.mse-include ^
+    sets-tvp.mse-export-template ^
+    magic-m15-altered-plus.mse-style ^
+    magic-m15-altered.mse-style
+
+
+exit
